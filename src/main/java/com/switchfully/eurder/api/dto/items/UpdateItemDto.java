dba3e7fcbbfd25596d151a4d0db2165f911cbba7
Klaas Devits <klaas.devits@gmail.com>
2021-10-29 10:46:23 +0200

    udateItem removed id field

--- src/main/java/com/switchfully/eurder/api/dto/items/UpdateItemDto.java
+++ src/main/java/com/switchfully/eurder/api/dto/items/UpdateItemDto.java
@@ -1,7 +1,37 @@
 package com.switchfully.eurder.api.dto.items;
 
-public class UpdateItemDto extends ItemDto {
+public class UpdateItemDto {
+    private String name;
+    private String description;
+    private double price;
     private int stock;
+
+    public String getName() {
+        return name;
+    }
+
+    public UpdateItemDto setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public UpdateItemDto setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public UpdateItemDto setPrice(double price) {
+        this.price = price;
+        return this;
+    }
 
     public int getStock() {
         return stock;
